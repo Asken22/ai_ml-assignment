50fd0bbf955a39f868f4522932c847b6a37ce886
Gangeswara P S <psgangeswara@gmail.com>
2026-02-26 20:01:34 +0530

    I  pull the commited change and now i was commiting again

---
+++ config.py
@@ -0,0 +1,1 @@
+#I want to config this so i am doing right now
--- utils.py
+++ utils.py
@@ -2,5 +2,7 @@ FIRST_NO=int(input("Enter the first number: "))
 SECOND_NO=int(input("Enter the second number: "))
 
 Sum=FIRST_NO+SECOND_NO
+Sub=FIRST_NO-SECOND_NO
 
 print(f"The sum of the two numbers is {Sum}")
+print(f"The difference of the two numbers is {Sub}")
